45776c24dcf7584d5956519cdf2b8669abcd5bf8
Chen Xu <cxu264@usc.edu>
2018-03-22 19:58:22 -0700

    finished version 1

--- auto_completion/src/main/java/proj/auto/complete/DBOutputWritable.java
+++ auto_completion/src/main/java/proj/auto/complete/DBOutputWritable.java
@@ -7,11 +7,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBOutputWritable implements DBWritable{
-
-    public void readFields(ResultSet arg0) throws SQLException{
-
+    private String start_word="";
+    private String follow_word="";
+    private int count=0;
+    public DBOutputWritable(String s,String f,int c){
+        this.start_word=s;
+        this.follow_word=f;
+        this.count=c;
     }
-    public void write(PreparedStatement arg0) throws SQLException{
-
+    public void readFields(ResultSet result) throws SQLException{
+          this.start_word=result.getString("starting_phrase");
+          this.follow_word=result.getString("following_word");
+          this.count=result.getInt("count");
+    }
+    public void write(PreparedStatement statement) throws SQLException{
+          statement.setString(1,this.start_word);
+          statement.setString(2,this.follow_word);
+          statement.setInt(3,this.count);
     }
 }
--- auto_completion/src/main/java/proj/auto/complete/Driver.java
+++ auto_completion/src/main/java/proj/auto/complete/Driver.java
@@ -4,10 +4,12 @@ import org.apache.hadoop.conf.Configuration;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 
 import org.apache.hadoop.mapred.lib.db.DBConfiguration;
 import org.apache.hadoop.mapred.lib.db.DBOutputFormat;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -24,7 +26,7 @@ public class Driver{
         conf.set("N",args[2]);
         //the first job used to generate the n-gram
 
-        Job job1=Job.getInstance();
+        Job job1=Job.getInstance(conf);
         job1.setJobName("n_gram");
         job1.setJarByClass(Driver.class);
         //set job1's mapper class and reducer class
@@ -45,23 +47,29 @@ public class Driver{
         //start of job2
         Configuration  conf2=new Configuration();
         conf2.set("threshold",args[3]);
-
+        conf2.set("k",args[4]);
         DBConfiguration.configureDB(conf2,"com.mysql,jdbc.Driver",
-                "jdbc:mysql://127.0.0.1:port/test","root",
+                "jdbc:mysql://127.0.0.1:3306/test","root",
                 "Xuchen123");
 
-        Job job2=Job.getInstance();
+        Job job2=Job.getInstance(conf2);
         job2.setJobName("language_model");
         job2.setJarByClass(Driver.class);
-        job2.addArchiveToClassPath(new Path("path_to_ur_connector"));
+        job2.addArchiveToClassPath(new Path("args[5]"));
 
         job2.setMapOutputKeyClass(Text.class);
         job2.setMapOutputValueClass(Text.class);
 
-        job2.setOutputKeyClass(TextInputFormat.class);
-        job2.setOutputValueClass(DBOutputFormat.class);
+        job2.setOutputKeyClass(DBOutputWritable.class);
+        job2.setOutputValueClass(NullWritable.class);
 
         job2.setMapperClass(LanguageModel.LanguageMap.class);
         job2.setReducerClass(LanguageModel.LanguageReducer.class);
+        job2.setInputFormatClass(TextInputFormat.class);
+        job2.setOutputFormatClass(DBOutputFormat.class);
+
+        DBOutputFormat.setOutput(job2,"output",new String[]{"starting_phrase","following_word","count"});
+        TextInputFormat.setInputPaths(job2,args[1]);
+        job2.waitForCompletion(true);
     }
 }
